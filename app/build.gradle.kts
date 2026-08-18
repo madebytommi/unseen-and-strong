06ee3c0f464ff89f71fd43c0c4262faf04007921
Tommi <9156878+madebytommi@users.noreply.github.com>
2026-08-18 07:34:50 -0500

    chore: target Android API 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.unseenandstrong"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.madebytommi.unseenandstrong"
         minSdk = 29
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
