241c7824863e137852ca9eff4f79f5d7bfa89d24
Tommi <9156878+madebytommi@users.noreply.github.com>
2026-08-18 18:14:16 -0500

    chore: keep branding build changes scoped

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
-    id("com.google.devtools.ksp") version "2.3.6"
+    id("com.google.devtools.ksp") version "2.3.2"
 }
 
 android {
